0692bcbce72038f9260379194fbda5a9e3afea03
lxx-ue <71316369+lxx-ue@users.noreply.github.com>
2022-08-02 22:47:34 +0300

    add logic

--- test_saber_interactive.cpp
+++ test_saber_interactive.cpp
@@ -12,14 +12,37 @@ class ListRand {
 public:
     ListNode* head = nullptr;
     ListNode* tail = nullptr;
+    ListNode* current = nullptr;
     
     int count = 0;
 
     void Serialize();
     void Deserialize();
+
+    ListNode* addItem(const std::string data)
+    {
+        ListNode* node = new ListNode;
+        node->data = data;
+        if (head == 0)
+        {
+            head = current = tail = node;
+        }
+        else
+        {
+            current->next = node;
+            node->prev = current;
+            tail = current = node;
+        }
+        ++count;
+        return node;
+    }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ListRand list;
+    list.addItem("green");
+    list.addItem("orange");
+    list.addItem("red");
+
 }
