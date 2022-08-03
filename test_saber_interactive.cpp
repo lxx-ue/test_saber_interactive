7f67d91f382bb6128fb1a46f1ae195a3fa39dbfd
lxx-ue <71316369+lxx-ue@users.noreply.github.com>
2022-08-03 23:30:08 +0300

    serialization ready

--- test_saber_interactive.cpp
+++ test_saber_interactive.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <unordered_map>
 
 #include <time.h>
 
@@ -23,12 +24,24 @@ public:
 
 	void Serialize(std::ofstream* out)
 	{
-		ListNode* node = head;
-		while (node)
+		std::unordered_map<ListNode*, int> m;
+		ListNode* current = head;
+		int pos = 0;
+		while (current)
 		{
-			*out << node->data << ' ' << rand() % count;
+			m.insert(std::make_pair(current, pos));
+			++pos;
+			current = current->next;
+		}
+		current = head;
+		while (current)
+		{
+			*out << current->data << ' ';
+			if (current->rand) *out << m[current->rand];
+			else *out << -1;
+			//*out << node->rand ? m[node->rand] : -1;
 			*out << '\n';
-			node = node->next;
+			current = current->next;
 		}
 	}
 
@@ -68,41 +81,52 @@ public:
 
 	void setRands()
 	{
+		std::vector<ListNode*> m;
 		ListNode* node = head;
 		while (node)
 		{
+			m.push_back(node);
 			node = node->next;
+		}
+		node = head;
+		int pos = 0;
+		while (node)
+		{
+			int r = rand() % count;
+			node->rand = m.at(r);
+			node = node->next;
+			++pos;
 		}
 	}
 
 	void print()
 	{
 		ListNode* node = head;
-		//while (node)
-		//{
-		//	std::cout << node->data << '\t' << node << '\t' << node->prev <<'\t'<<node->next << '\t' << node->rand << std::endl;
-		//	node = node->next;
-		//}
-		int i = 0;
 		while (node)
 		{
-			int r = rand() % count;
-			std::cout << i << '\t' << node->data << '\t' << node << '\t' << r;// << std::endl;
-			
-			ListNode* hnode = head;
-			int j = 0;
-			while (hnode)
-			{
-
-				if (j == r) std::cout << ' ' << hnode << std::endl;
-				hnode = hnode->next;
-				j++;
-			}
-			
+			std::cout << node->data << '\t' << node << '\t' << node->prev <<'\t'<<node->next << '\t' << node->rand << std::endl;
 			node = node->next;
-			++i;
-
 		}
+		//int i = 0;
+		//while (node)
+		//{
+		//	int r = rand() % count;
+		//	std::cout << i << '\t' << node->data << '\t' << node << '\t' << r;// << std::endl;
+		//	
+		//	ListNode* hnode = head;
+		//	int j = 0;
+		//	while (hnode)
+		//	{
+
+		//		if (j == r) std::cout << ' ' << hnode << std::endl;
+		//		hnode = hnode->next;
+		//		j++;
+		//	}
+		//	
+		//	node = node->next;
+		//	++i;
+
+		//}
 	}
 };
 
@@ -111,15 +135,18 @@ int main()
 	srand(time(0));
 	ListRand list;
 	
-	//list.addItem("green");
-	//list.addItem("orange");
-	//list.addItem("red");
-	//list.addItem("black");
-	//list.addItem("white");
-	//std::ofstream fout("tt.txt");
-	//list.Serialize(&fout);
+	list.addItem("green");
+	list.addItem("orange");
+	list.addItem("red");
+	list.addItem("black");
+	list.addItem("white");
+	std::ofstream fout("tt.txt");
 
-	std::ifstream fin2("tt.txt");
-	list.Deserialize(&fin2);
+	list.setRands();
+	list.Serialize(&fout);
 	list.print();
+
+	//std::ifstream fin2("tt.txt");
+	//list.Deserialize(&fin2);
+	//list.print();
 }
